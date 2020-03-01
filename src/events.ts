77ebc89d40731ab553ae3659b11541e8f410820b
webstrand <webstrand@gmail.com>
2020-02-29 23:46:56 -0500

    add Stats event for monitors

--- src/events.ts
+++ src/events.ts
@@ -2,6 +2,7 @@ export const SecretRoom = "d74c8beb-aa3b-49f6-9753-0b799d7b3a56";
 
 export enum FireflyEvent {
 	State = "state",
+	Stats = "stats",
 	Info = "info",
 	Signon = "af2fa2cd-3eea-4667-9040-bbfa40f7d540",
 	Admin = "admin",
@@ -10,7 +11,8 @@ export enum FireflyEvent {
 
 
 export type FireflyEventMap = {
-	"state": { current_category: number, voting_is_open: boolean }
+	"state": { current_category: number, voting_is_open: boolean };
+	"stats": { category: number, votes: number[] };
 	"info": string;
 	"af2fa2cd-3eea-4667-9040-bbfa40f7d540": "helo" | "stats";
 	"admin": "open_category" | "close_category" | "next_category" | "prev_category" | "reset_category" | "fatal_reset";
