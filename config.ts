f3fe2c5bc586c5a94e4b3d4a80765d0594624715
webstrand <webstrand@gmail.com>
2021-03-08 18:22:37 -0500

    use declare const enum to avoid leaking under --isolatedModules

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@
  * Event identifiers used to denote message types. There's no security advantage
  * to using random event identifiers, merely obfuscation.
  */
-export const enum Event {
+export declare const enum Event {
 	State = "65996b32-0f72-4522-a6ee-8266464628b8",
 	Stats = "1c3a80d6-8210-40de-8a25-e6ed32793d95",
 	Info = "294bd6f6-280c-4a76-822c-f56783b7dab2",
@@ -14,10 +14,10 @@ export const enum Event {
 /**
  * Secrets that are shared between specific clients and the server.
  *
- * WARNING: The enum below *MUST* be const, otherwise these secrets
- * will leak to all clients.
+ * WARNING: The enum below *MUST* be declare const, otherwise these secrets
+ * might leak to all clients.
  */
-export const enum Secret {
+export declare const enum Secret {
 	MonitorSignon = "ae0508da-9120-4ba2-b7e5-eeec0ed73af1",
 	AdminSignon = "c1123695-6422-41f8-8bc6-ee31768ebe95"
 }
